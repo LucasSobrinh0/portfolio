dd821fd852b5a4d045d6188b1dbef51ef7df9988
lucassobrinh0 <lucas.sobrinho.crn@gmail.com>
2025-01-13 14:53:32 -0400

    Adicionando estilos aos botões do nav-links

--- script.js
+++ script.js
@@ -36,3 +36,32 @@ document.addEventListener('DOMContentLoaded', () => {
   // Exibe a seção de projetos por padrão
   showSection(pagesSection);
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.nav-link');
+    const sections = document.querySelectorAll('.content');
+  
+    function showSection(sectionToShow) {
+      sections.forEach((section) => section.classList.add('hidden'));
+      sectionToShow.classList.remove('hidden');
+    }
+  
+    function setActiveButton(activeButton) {
+      buttons.forEach((button) => button.classList.remove('active'));
+      activeButton.classList.add('active');
+    }
+  
+    buttons.forEach((button) => {
+      button.addEventListener('click', (event) => {
+        const targetSection = document.querySelector(`#${event.target.id.replace('btn-', '')}-section`);
+        showSection(targetSection);
+        setActiveButton(event.target);
+      });
+    });
+  
+    // Exibe a seção de projetos por padrão
+    const defaultButton = document.getElementById('btn-pages');
+    showSection(document.getElementById('pages-section'));
+    setActiveButton(defaultButton);
+  });
+  
